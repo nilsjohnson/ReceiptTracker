0f20a5fa048465a614eb3ba2ee6fa2c93e7f38d2
Nils Johnson <nilsjohnson328@gmail.com>
2020-02-09 12:55:26 -0500

    polished up the 'there are no receipts : (' view

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -2,11 +2,8 @@
 #include <string>
 
 #include <opencv2/core/core.hpp>
-#include <opencv2/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
 #include "opencv2/imgproc/imgproc.hpp"
-//#include <iostream>//
-//#include <string>
+
 
 using namespace cv;
 using namespace std;
