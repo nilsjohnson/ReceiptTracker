be337761acab763d71d3395b56fc41f6c1887404
Nils <nilsjohnson328@gmail.com>
2020-06-12 11:49:58 -0400

    implemented auto rotate

---
+++ app/src/main/cpp/ReceiptCropper.h
@@ -0,0 +1,45 @@
+/*
+ * ReceiptCropper.h
+ *
+ *  Created on: May 22, 2020
+ *      Author: nils
+ */
+
+#ifndef RECEIPTCROPPER_H_
+#define RECEIPTCROPPER_H_
+
+#include <opencv2/core/core.hpp>
+
+using namespace std;
+using namespace cv;
+// if DEBUG, you may print images for each step of the way.
+const string DEBUG_OUT_DIR = "/home/nils/eclipse-cpp-ws/receipt-cropper/out/";
+const bool DEBUG = false;
+
+
+class ReceiptCropper {
+private:
+    // The original receipt
+    Mat* receipt;
+    // the cropped receipt
+    Mat* cropped;
+    // values to set the threshold to when finding the receipt
+    const int THRESH_BRIGHT = 200;
+    const int THRESH_NORMAL = 150;
+
+    // max brightness level before toggle to THRESH_BRIGHT
+    const int BRIGHT = 110;
+    bool hasBrightEdges(Mat* mat);
+
+    void swap(int &a, int &b);
+    static bool compareContourAreas(vector<Point> contour_1, vector<Point> contour_2);
+    void crop(Mat* mat, bool brightEdges);
+    void setPortrait();
+public:
+    ReceiptCropper(Mat* receipt);
+    Mat* getCropped();
+
+    virtual ~ReceiptCropper();
+};
+
+#endif /* RECEIPTCROPPER_H_ */
--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,43 +1,24 @@
 #include <jni.h>
 #include <opencv2/core/core.hpp>
+#include <vector>
 #include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+
+#include "ReceiptCropper.h"
+
 
 using namespace cv;
+using namespace std;
 
-bool compareContourAreas(std::vector<cv::Point> contour1, std::vector<cv::Point> contour2)
-{
-    double i = fabs(contourArea(cv::Mat(contour1)));
-    double j = fabs(contourArea(cv::Mat(contour2)));
-    return (i > j);
-}
 
 extern "C"
 JNIEXPORT jlong JNICALL
 Java_coffee_nils_dev_receipts_util_ImageUtil_autoCrop__J(JNIEnv *env, jclass thiz, jlong addr)
 {
-    Mat* image = (Mat*) addr;
+    Mat* image = (Mat*)addr;
+    ReceiptCropper* cropper = new ReceiptCropper(image);
+    image = cropper->getCropped();
+    delete cropper;
 
-    Rect bounding_rect;
-
-    Mat thresh(image->rows, image->cols, CV_8UC1);
-    cvtColor(*image, thresh, COLOR_RGB2GRAY); // convert gray
-    threshold(thresh, thresh, 150, 255, THRESH_BINARY + THRESH_OTSU); //Threshold the gray
-
-    // Vector for storing contour
-    std::vector<std::vector<Point>> contours;
-    std::vector<Vec4i> hierarchy;
-    findContours(thresh, contours, hierarchy, RETR_CCOMP, CHAIN_APPROX_SIMPLE);
-    sort(contours.begin(), contours.end(), compareContourAreas); //Store the index of largest contour
-
-    bounding_rect = boundingRect((const _InputArray &)contours[0]);
-    rectangle(*image, bounding_rect, Scalar(250, 250, 250), 3);
-    Mat* croppedImage = new Mat(*image, bounding_rect);//image(bounding_rect);
-
-    // if landscape
-    if(croppedImage->cols > croppedImage->rows)
-    {
-        rotate(*croppedImage, *croppedImage, ROTATE_90_CLOCKWISE);
-    }
-
-    return (jlong)croppedImage;
+    return (jlong)image;
 }
